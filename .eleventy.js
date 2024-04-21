7b2f3554915b2acc7b920cf2596cccd4aa2b59b4
Sira <si.ecenk@gmail.com>
2024-04-21 19:49:33 +0700

    remove lazyloading

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,3 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("asset");
     eleventyConfig.addPassthroughCopy("all.js")
   };
-
-const lazyloadPlugin = require('eleventy-plugin-lazyload')
-
-module.exports = function (eleventyConfig) {
-	eleventyConfig.addPlugin(lazyloadPlugin, {
-		alt: false, // if enabled, adds an empty alt attribute on images that don't have it
-	})
-}
