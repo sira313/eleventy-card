ef720070b505074f2d083bb28f89b5d46d354212
Sira <si.ecenk@gmail.com>
2024-04-21 21:50:05 +0700

    revert back, and add liquid date template

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,4 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("asset");
     eleventyConfig.addPassthroughCopy("all.js");
-    eleventyConfig.addFilter("dateFmt", (value) => {
-        return value.toISOString().split("T")[0];
-    });
   };
