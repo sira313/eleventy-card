c6db54f4245da45131d3053f91dd78a90f87c2ef
Sira <si.ecenk@gmail.com>
2024-04-21 18:19:16 +0700

    lazy image test in /photos

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
-    eleventyConfig.addPassthroughCopy("asset")
+    eleventyConfig.addPassthroughCopy("asset");
+    eleventyConfig.addPassthroughCopy("all.js")
   };
---
+++ all.js
@@ -0,0 +1,7 @@
+// idk what i am doing here wtf
+
+$(document).ready(function(){
+    $('img.lazy').lazyload({
+      effect: "fadeIn"
+    });
+  });
