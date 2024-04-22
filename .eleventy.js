ca0ab86a33fdfb491094c508a37bf0f245f34f86
Sira <si.ecenk@gmail.com>
2024-04-22 17:56:22 +0700

    revert back

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
+const { execSync } = require('child_process')
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("style.css");
-    eleventyConfig.addPassthroughCopy("asset");
-    eleventyConfig.addPassthroughCopy("all.js");
-  };
+  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("asset");
+  eleventyConfig.addPassthroughCopy("all.js");
+}
