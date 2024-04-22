b01c2d3f61edf389d4402900ebbb9d86b7c5b0eb
Sira <si.ecenk@gmail.com>
2024-04-23 00:47:34 +0700

    lazy image

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 const tinyHTML = require('@sardine/eleventy-plugin-tinyhtml');
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(lazyImagesPlugin);
   eleventyConfig.addPlugin(tinyHTML);
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("asset");
