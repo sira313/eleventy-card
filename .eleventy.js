03a44a6fc9fe45d9a03f70fbb9dc037fcc4bc1d1
Sira Argia <45110036+sira313@users.noreply.github.com>
2024-05-09 18:34:13 +0700

    Page url (#82)
    * simple share button
    
    * share button photos

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ const eleventyPluginFeathericons = require('eleventy-plugin-feathericons');
 const { execSync } = require('child_process')
 
 module.exports = function (eleventyConfig) {
+	// change this rootURL with your domain (without / at the end, see my example) !important
+	eleventyConfig.addGlobalData("rootURL", "https://eleventy-card.netlify.app");
 	eleventyConfig.on('eleventy.after', () => {
 		execSync(`npx pagefind --source _site --glob \"**/*.html\"`, { encoding: 'utf-8' })
 	  })
